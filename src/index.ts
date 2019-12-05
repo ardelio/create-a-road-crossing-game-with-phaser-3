2a951c681211516609031d0602332b5d188cf6e6
Anthony Sceresini <anthony.sceresini@myob.com>
2019-12-05 22:22:15 +1100

    Add enemy with movement and speed

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,13 @@ const ASSETS = {
 
 export default class GameScene extends Phaser.Scene {
   private player: Phaser.GameObjects.Sprite;
+  private enemy: Phaser.GameObjects.Sprite;
   private treasure: Phaser.GameObjects.Sprite;
   private playerSpeed: number;
+  private enemyMinSpeed: number;
+  private enemyMaxSpeed: number;
+  private enemyMinY: number;
+  private enemyMaxY: number;
 
   constructor() {
     super('Game');
@@ -18,11 +23,16 @@ export default class GameScene extends Phaser.Scene {
 
   init() {
     this.playerSpeed = 3;
+    this.enemyMinSpeed = 2;
+    this.enemyMaxSpeed = 4.5;
+    this.enemyMinY = 80;
+    this.enemyMaxY = 280;
   }
 
   preload() {
     this.load.image('background', ASSETS.BACKGROUND);
     this.load.image('player', ASSETS.PLAYER);
+    this.load.image('dragon', ASSETS.DRAGON);
     this.load.image('treasure', ASSETS.TREASURE);
 
   }
@@ -38,6 +48,13 @@ export default class GameScene extends Phaser.Scene {
 
     this.treasure = this.add.sprite(gameWidth - 80, gameHeight / 2, 'treasure');
     this.treasure.setScale(0.6);
+
+    this.enemy = this.add.sprite(120, gameHeight / 2, 'dragon');
+    this.enemy.flipX = true;
+    this.enemy.setScale(0.6);
+    const direction = Math.random() < 0.5 ? 1 : -1;
+    const speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+    (this.enemy as any).speed = direction * speed;
   }
 
   update() {
@@ -51,6 +68,15 @@ export default class GameScene extends Phaser.Scene {
     if (Phaser.Geom.Intersects.RectangleToRectangle(playerBounds, treasureBounds)) {
       console.log('Reached the treasure!')
       this.scene.restart();
+    }
+
+    this.enemy.y += (this.enemy as any).speed;
+
+    const conditionUp = (this.enemy as any).speed < 0 && this.enemy.y <= this.enemyMinY;
+    const conditionDown = (this.enemy as any).speed > 0 && this.enemy.y >= this.enemyMaxY
+
+    if (conditionDown || conditionUp) {
+      (this.enemy as any).speed *= -1;
     }
   }
 }
